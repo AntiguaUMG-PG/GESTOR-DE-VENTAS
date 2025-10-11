67ff9756db06fc5b364da797dbdbbcec41c63041
NathaIsmatul <esdrasismatul@hotmail.com>
2025-10-10 21:07:56 -0600

    actualizacion de main

--- PG- Distribuidora Sebaot/ModeloApi.py
+++ PG- Distribuidora Sebaot/ModeloApi.py
@@ -1965,10 +1965,5 @@ async def api_info():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(
-        "ModeloApi:app",  # Asume que el archivo se llama main.py
-        host="127.0.0.1",
-        port=8000,
-        reload=True,
-        log_level="info"
-    )
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
